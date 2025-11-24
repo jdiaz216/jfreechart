b091ca424ec3b5a01e1cb62e18edf444b286cd01
Jorge Diaz <jldiaz@dminc.com>
2025-11-23 21:50:17 -0500

    Extract common logic into protected methods

--- source/org/jfree/chart/renderer/category/LineAndShapeRenderer.java
+++ source/org/jfree/chart/renderer/category/LineAndShapeRenderer.java
@@ -899,74 +899,24 @@ public class LineAndShapeRenderer extends AbstractCategoryItemRenderer
             ValueAxis rangeAxis, CategoryDataset dataset, int row, int column,
             int pass) {
 
-        // do nothing if item is not visible
-        if (!getItemVisible(row, column)) {
-            return;
-        }
-
-        // nothing is drawn for null...
-        Number v = dataset.getValue(row, column);
-        if (v == null) {
+        if (notValidRowAndColumn(dataset, row, column)) {
             return;
         }
 
         // do nothing if both the line and shape are not visible
-        if (!getItemLineVisible(row, column)
-                && !getItemShapeVisible(row, column)) {
+        if (!getItemLineVisible(row, column) && !getItemShapeVisible(row, column)) {
             return;
         }
 
+        Number v = dataset.getValue(row, column);
         PlotOrientation orientation = plot.getOrientation();
 
         // current data point...
-        double x1;
-        if (getUseSeriesOffset()) {
-            x1 = domainAxis.getCategorySeriesMiddle(dataset.getColumnKey(
-                    column), dataset.getRowKey(row), dataset, getItemMargin(),
-                    dataArea, plot.getDomainAxisEdge());
-        }
-        else {
-            x1 = domainAxis.getCategoryMiddle(column, getColumnCount(),
-                    dataArea, plot.getDomainAxisEdge());
-        }
-
-        double y1 = rangeAxis.valueToJava2D(v.doubleValue(), dataArea,
-                plot.getRangeAxisEdge());
+        double x1 = getX1(dataArea, plot, domainAxis, dataset, row, column);
+        double y1 = getY1(dataArea, plot, rangeAxis, v);
 
         if (pass == 0 && getItemLineVisible(row, column)) {
-            if (column != 0) {
-                Number previousValue = dataset.getValue(row, column - 1);
-                if (previousValue != null) {
-                    // previous data point...
-                    double previous = previousValue.doubleValue();
-                    double x0;
-                    if (getUseSeriesOffset()) {
-                        x0 = domainAxis.getCategorySeriesMiddle(
-                                dataset.getColumnKey(column - 1),
-                                dataset.getRowKey(row), dataset,
-                                getItemMargin(), dataArea,
-                                plot.getDomainAxisEdge());
-                    }
-                    else {
-                        x0 = domainAxis.getCategoryMiddle(column - 1,
-                                getColumnCount(), dataArea,
-                                plot.getDomainAxisEdge());
-                    }
-                    double y0 = rangeAxis.valueToJava2D(previous, dataArea,
-                            plot.getRangeAxisEdge());
-
-                    Line2D line = null;
-                    if (orientation == PlotOrientation.HORIZONTAL) {
-                        line = new Line2D.Double(y0, x0, y1, x1);
-                    }
-                    else if (orientation == PlotOrientation.VERTICAL) {
-                        line = new Line2D.Double(x0, y0, x1, y1);
-                    }
-                    g2.setPaint(getItemPaint(row, column));
-                    g2.setStroke(getItemStroke(row, column));
-                    g2.draw(line);
-                }
-            }
+            drawLine(g2, dataArea, plot, domainAxis, rangeAxis, dataset, row, column, orientation, y1, x1);
         }
 
         if (pass == 1) {
@@ -1006,25 +956,94 @@ public class LineAndShapeRenderer extends AbstractCategoryItemRenderer
                     dataset.getRowKey(row), dataset.getColumnKey(column),
                     v.doubleValue(), datasetIndex, x1, y1, orientation);
 
-            // draw the item label if there is one...
-            if (isItemLabelVisible(row, column)) {
-                if (orientation == PlotOrientation.HORIZONTAL) {
-                    drawItemLabel(g2, orientation, dataset, row, column, y1,
-                            x1, (v.doubleValue() < 0.0));
-                }
-                else if (orientation == PlotOrientation.VERTICAL) {
-                    drawItemLabel(g2, orientation, dataset, row, column, x1,
-                            y1, (v.doubleValue() < 0.0));
-                }
-            }
+            drawLabel(g2, dataset, row, column, orientation, y1, x1, v);
+            addItemEntity(state, dataset, row, column, shape);
+        }
+    }
 
-            // add an item entity, if this information is being collected
-            EntityCollection entities = state.getEntityCollection();
-            if (entities != null && shape != null) {
-                addItemEntity(entities, dataset, row, column, shape);
+    protected boolean notValidRowAndColumn(CategoryDataset dataset, int row, int column) {
+        return !getItemVisible(row, column) || dataset.getValue(row, column) == null;
+    }
+
+    protected double getY1(Rectangle2D dataArea, CategoryPlot plot, ValueAxis rangeAxis, Number v) {
+        return rangeAxis.valueToJava2D(v.doubleValue(), dataArea,
+                plot.getRangeAxisEdge());
+    }
+
+    protected double getX1(Rectangle2D dataArea, CategoryPlot plot, CategoryAxis domainAxis, CategoryDataset dataset,
+                         int row, int column) {
+        double x1;
+        if (getUseSeriesOffset()) {
+            x1 = domainAxis.getCategorySeriesMiddle(dataset.getColumnKey(
+                            column), dataset.getRowKey(row), dataset, getItemMargin(),
+                    dataArea, plot.getDomainAxisEdge());
+        }
+        else {
+            x1 = domainAxis.getCategoryMiddle(column, getColumnCount(),
+                    dataArea, plot.getDomainAxisEdge());
+        }
+        return x1;
+    }
+
+    protected void addItemEntity(CategoryItemRendererState state, CategoryDataset dataset, int row, int column, Shape shape) {
+        // add an item entity, if this information is being collected
+        EntityCollection entities = state.getEntityCollection();
+        if (entities != null && shape != null) {
+            addItemEntity(entities, dataset, row, column, shape);
+        }
+    }
+
+    protected void drawLabel(Graphics2D g2, CategoryDataset dataset, int row, int column, PlotOrientation orientation,
+                           double y1, double x1, Number v) {
+        // draw the item label if there is one...
+        if (isItemLabelVisible(row, column)) {
+            if (orientation == PlotOrientation.HORIZONTAL) {
+                drawItemLabel(g2, orientation, dataset, row, column, y1,
+                        x1, (v.doubleValue() < 0.0));
+            }
+            else if (orientation == PlotOrientation.VERTICAL) {
+                drawItemLabel(g2, orientation, dataset, row, column, x1,
+                        y1, (v.doubleValue() < 0.0));
             }
         }
+    }
 
+    protected void drawLine(Graphics2D g2, Rectangle2D dataArea, CategoryPlot plot, CategoryAxis domainAxis,
+                           ValueAxis rangeAxis, CategoryDataset dataset, int row, int column,
+                           PlotOrientation orientation, double y1, double x1) {
+        if (column != 0) {
+            Number previousValue = dataset.getValue(row, column - 1);
+            if (previousValue != null) {
+                // previous data point...
+                double previous = previousValue.doubleValue();
+                double x0;
+                if (getUseSeriesOffset()) {
+                    x0 = domainAxis.getCategorySeriesMiddle(
+                            dataset.getColumnKey(column - 1),
+                            dataset.getRowKey(row), dataset,
+                            getItemMargin(), dataArea,
+                            plot.getDomainAxisEdge());
+                }
+                else {
+                    x0 = domainAxis.getCategoryMiddle(column - 1,
+                            getColumnCount(), dataArea,
+                            plot.getDomainAxisEdge());
+                }
+                double y0 = rangeAxis.valueToJava2D(previous, dataArea,
+                        plot.getRangeAxisEdge());
+
+                Line2D line = null;
+                if (orientation == PlotOrientation.HORIZONTAL) {
+                    line = new Line2D.Double(y0, x0, y1, x1);
+                }
+                else if (orientation == PlotOrientation.VERTICAL) {
+                    line = new Line2D.Double(x0, y0, x1, y1);
+                }
+                g2.setPaint(getItemPaint(row, column));
+                g2.setStroke(getItemStroke(row, column));
+                g2.draw(line);
+            }
+        }
     }
 
     /**
